f1d42651659048f14d8c7a0c6c257fa517331d0b
Florentino Navarro <tino.navarro@hotmail.com>
2023-05-18 18:00:35 -0600

    mobile

--- src/components/templates/AppTemplate.tsx
+++ src/components/templates/AppTemplate.tsx
@@ -1,3 +1,4 @@
+//@ts-nocheck
 import { Outlet, useNavigate } from "react-router-dom";
 import Header from "../organisms/Header";
 import Footer from "../organisms/Footer";
@@ -22,6 +23,30 @@ const AppTemplate = () => {
       // offset: 250,
       // once: true,
     });
+    // const colLeftHight = document.querySelector('.img-container-color').clientHeight;
+    const colLeftHight = document.querySelector('.col-left').clientHeight;
+    const logo = document.querySelector('.Header__logo img')
+    const Hamburger = document.querySelector('.hamburger')
+    console.log(logo);
+    // console.log(colLeftHight)
+    window.addEventListener('scroll', ()=>{
+
+      let scroll = window.scrollY;
+      // console.log(scroll, colLeftHight);
+      if(!document.body.classList.contains('page-home')){
+      if (scroll>colLeftHight){
+        // alert("Scroll");
+        document.querySelector('header').classList.add('header-background')
+        Hamburger.classList.add('scroll')
+        logo.src='/logo.svg'
+      }else{
+        document.querySelector('header').classList.remove('header-background')
+        Hamburger.classList.remove('scroll')
+        logo.src='/logo-light.svg'
+
+      }
+    }
+    })
   }, []);
   return (
     <>
