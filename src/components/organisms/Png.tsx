dd04332bddc3f58b8bb9e10db4b0269f949b0711
Florentino Navarro <tino.navarro@hotmail.com>
2023-05-19 10:10:59 -0600

    mobile

--- src/components/organisms/Png.tsx
+++ src/components/organisms/Png.tsx
@@ -6,10 +6,10 @@ type Props = {
 const Png = ({ img }: Props) => {
   return (
     <picture>
-      <source media="(min-width: 2200px)" srcSet={`/nuevas/${img}.png`} />
-      <source media="(min-width: 1024px)" srcSet={`/nuevas/large/${img}-l.png`} />
-      <source media="(min-width: 100px)" srcSet={`/nuevas/samall/${img}-s.png`} />
-      <img src={`/nuevas/samall/${img}-s.png`} alt={img} />
+      <source media="(min-width: 2200px)" srcSet={`/botellas/${img}@4x.png`} />
+      <source media="(min-width: 1024px)" srcSet={`/botellas/${img}@3x.png`} />
+      <source media="(min-width: 100px)" srcSet={`/botellas/${img}@2x.png`} />
+      <img src={`/botellas/${img}@3x.png`} alt={img} />
     </picture>
   )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,21 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import { qrcode } from 'vite-plugin-qrcode';
 import removeConsole from 'vite-plugin-remove-console';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     removeConsole(),
-    qrcode()
+    qrcode(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: '_redirects',
+          dest: '',
+        },
+      ],
+    }),
   ],
 })
